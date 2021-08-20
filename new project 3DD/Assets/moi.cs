2e704e239c5d842aee4efbd964839c29afbfaecd
Leobold08 <leonivakoski1@gmail.com>
2021-08-20 19:47:49 +0300

    hi
    hi

---
+++ new project 3DD/Assets/moi.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       moi 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
